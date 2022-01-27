cffec27d012b1f60a6c7c2aae04a9b3f873e2675
sileeee <tldms201@naver.com>
2022-01-28 00:15:18 +0900

    feat: 프로젝트 생성

---
+++ src/main/java/com/seoridam/rehearserver/RehearServerApplication.java
@@ -0,0 +1,13 @@
+package com.seoridam.rehearserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RehearServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RehearServerApplication.class, args);
+    }
+
+}
