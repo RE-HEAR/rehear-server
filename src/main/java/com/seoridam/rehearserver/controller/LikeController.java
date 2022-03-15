ce44114659b8c930935c46e34c98e0f4366da136
sileeee <tldms201@naver.com>
2022-03-15 23:05:06 +0900

    fix: API 수정

--- src/main/java/com/seoridam/rehearserver/controller/LikeController.java
+++ src/main/java/com/seoridam/rehearserver/controller/LikeController.java
@@ -15,7 +15,7 @@ public class LikeController {
 
     private final LikeService likeService;
     
-    @PostMapping("/like")
+    @PostMapping("members/like")
     public void getInterview(@Valid @RequestBody LikeRequestDto dto, @AuthenticationPrincipal User user){
 
          likeService.postLike(dto, user);
