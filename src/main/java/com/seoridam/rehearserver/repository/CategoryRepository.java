0be1fe322132129475d06d3e47dc70218f0d9bfe
박계원 <62559049+Kyewon-Park@users.noreply.github.com>
2022-05-06 15:12:31 +0900

    feat: 카테고리 테이블 삭제

--- src/main/java/com/seoridam/rehearserver/repository/CategoryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.seoridam.rehearserver.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.seoridam.rehearserver.domain.Category;
-
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-}
